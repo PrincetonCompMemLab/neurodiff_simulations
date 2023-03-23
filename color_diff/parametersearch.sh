4661ab404351d6904ae08ee02f0617f7cf254d7b
anhhuyalex <alexn@minerva.kgi.edu>
2023-03-22 20:04:28 -0400

    documentation

--- color_diff/parametersearch.sh
+++ color_diff/parametersearch.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem-per-cpu=5G
 #SBATCH --time=00:50:00
 source activate leabra
-./main --mode=batch --saveDirName=./figs/test2/results_--HiddNumOverlapUnits=5 --runs=50 --trncyclog=false --tstcyclog=false                         --HiddNumOverlapUnits=5  
-python Post_analyses.py 50 ./figs/test2/results_--HiddNumOverlapUnits=5 cmd
+./main --mode=batch --saveDirName=./figs/test2/results_--HiddNumOverlapUnits=5 --runs=2 --trncyclog=false --tstcyclog=false                         --HiddNumOverlapUnits=5  
+python Post_analyses.py 2 ./figs/test2/results_--HiddNumOverlapUnits=5 cmd
 python read_task_parameters_into_csv.py ./figs/test2/results_--HiddNumOverlapUnits=5 cmd
 python cross_pair_analysis.py ./figs/test2 cmd
